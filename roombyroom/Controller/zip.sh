f91815d250e2c1cdcfc2ced3ed9205702932b196
Graham Trott <gtanyware@gmail.com>
2025-08-07 20:56:37 +0100

    250807.2

--- roombyroom/Controller/zip.sh
+++ roombyroom/Controller/zip.sh
@@ -5,3 +5,7 @@
 tar zcf /home/graham/dev/rbr/roombyroom/Server/rbrheating.com/ui/dist.tgz *
 cp setup /home/graham/dev/rbr/roombyroom/Server/rbrheating.com/ui
 cp version /home/graham/dev/rbr/roombyroom/Server/rbrheating.com/ui
+
+cd /home/graham/dev/rbr/roombyroom/Controller
+rm -rf ui/__pycache__
+tar zcf ui.tar.gz ui
